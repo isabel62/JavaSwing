1535508b9f9f5c4fa22b5a3dbbbaaa7da3b65fff
isabel62 <isabeliribarreng@hotmail.com>
2017-11-08 21:56:36 -0300

    todas las aplicaciones del programa ensolucion

--- src/javaswing/JavaSwing.java
+++ src/javaswing/JavaSwing.java
@@ -21,16 +21,37 @@ public class JavaSwing
     JLabel label;
     JTextField text;
     JButton boton;
+    JCheckbox
     //PASO 4-DEFINIR EL CONSTRUCTOR
     public window()
     {
-
+  
  
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {      
+       TODO code application logic here;
+       
+        window ventana = new window();//Creo un objeto tipo window
+        ventana.setVisible(true);
+        ventana.setBounds(0, 0,800, 800);
+        //La siguente inst.
+        ventana.setTitle("mi primera aplicacion con ventana");
+        //la siguente sentecia es para finalizar la vetana
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       
+    }         
+    
+   //PASO 5  CREAR E INICIALIZAR Los OBJETOS
+     
+ 
+        
+ 
+     
+    }
         // TODO code application logic here
     }
     
