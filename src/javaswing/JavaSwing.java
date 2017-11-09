6eb3884cee520a08d2e1292f278ab52f399de1cc
isabel62 <isabeliribarreng@hotmail.com>
2017-11-08 21:17:30 -0300

    IMPLEMENTAR PASO DEL PROGRAMA

---
+++ src/javaswing/JavaSwing.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaswing;
+
+//PASO 1-IMPLEMENTAR PASO UNPROGRAMA
+import javax.swing.*;//
+import java.awt.*;//
+
+/**
+ *
+ * @author LAB08
+ */
+//PASO 2 --HEREDAR LA CLASE DESDE JFRAME
+public class JavaSwing 
+{
+ //PASO 3-declarar elemento para 
+    JPanel panel;
+    JLabel label;
+    JTextField text;
+    JButton boton;
+    //PASO 4-DEFINIR EL CONSTRUCTOR
+    public window()
+    {
+
+ 
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
